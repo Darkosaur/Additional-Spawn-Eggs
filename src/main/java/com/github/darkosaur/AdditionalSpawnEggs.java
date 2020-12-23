6e5311888adcfdc406ebbdbd38719c15ccf5586c
DarkMethoDev <jurassicparkfan23@gmail.com>
2020-12-23 11:28:23 -0500

    Starting on code

---
+++ src/main/java/com/github/darkosaur/AdditionalSpawnEggs.java
@@ -0,0 +1,27 @@
+package com.github.darkosaur;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(AdditionalSpawnEggs.MOD_ID)
+public class AdditionalSpawnEggs
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "additional_spawn_eggs";
+
+    public AdditionalSpawnEggs() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) { }
+
+    private void doClientStuff(final FMLClientSetupEvent event) { }
+}
