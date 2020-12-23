8c6ba27d91ea592c71eb8d33fef79b72c4ec48ea
DarkMethoDev <jurassicparkfan23@gmail.com>
2020-12-23 12:57:11 -0500

    Add spawn egg for ender dragon, and snow golem

--- src/main/java/com/github/darkosaur/item/AdditionalSpawnEggsItemRegistry.java
+++ src/main/java/com/github/darkosaur/item/AdditionalSpawnEggsItemRegistry.java
@@ -16,4 +16,10 @@ public class AdditionalSpawnEggsItemRegistry {
     // Spawn Eggs
     public static final RegistryObject<SpawnEggItem> IRON_GOLEM_SPAWN_EGG = ITEMS.register("iron_golem_spawn_egg",
             () -> new SpawnEggItem(EntityType.IRON_GOLEM, 0xCBCAC5, 0x1AB61C, new Item.Properties().group(ItemGroup.MATERIALS)));
+
+    public static final RegistryObject<SpawnEggItem> SNOW_GOLEM_SPAWN_EGG = ITEMS.register("snow_golem_spawn_egg",
+            () -> new SpawnEggItem(EntityType.SNOW_GOLEM, 0xFFFFFF, 0xD4A81E, new Item.Properties().group(ItemGroup.MATERIALS)));
+
+    public static final RegistryObject<SpawnEggItem> ENDER_DRAGON_SPAWN_EGG = ITEMS.register("ender_dragon_spawn_egg",
+            () -> new SpawnEggItem(EntityType.ENDER_DRAGON, 0x151102, 0x07179F, new Item.Properties().group(ItemGroup.MATERIALS)));
 }
