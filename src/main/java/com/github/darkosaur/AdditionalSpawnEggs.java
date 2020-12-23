1bdd6f03e5b7b526cd1440c2af67344a2e60a69a
DarkMethoDev <jurassicparkfan23@gmail.com>
2020-12-23 12:52:37 -0500

    Add more stuff to main class

--- src/main/java/com/github/darkosaur/AdditionalSpawnEggs.java
+++ src/main/java/com/github/darkosaur/AdditionalSpawnEggs.java
@@ -1,27 +1,29 @@
 package com.github.darkosaur;
 
+import com.github.darkosaur.item.AdditionalSpawnEggsItemRegistry;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(AdditionalSpawnEggs.MOD_ID)
-public class AdditionalSpawnEggs
-{
+public class AdditionalSpawnEggs {
+
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "additional_spawn_eggs";
 
     public AdditionalSpawnEggs() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        final IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modBus.addListener(this::setup);
+
+        AdditionalSpawnEggsItemRegistry.ITEMS.register(modBus);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event) { }
-
-    private void doClientStuff(final FMLClientSetupEvent event) { }
 }
